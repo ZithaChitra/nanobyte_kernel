a564ce90f0d62a72de1e32f585410071c43ea766
northp <realblessingchitra@gmail.com>
2022-09-09 20:34:00 +0200

    update

--- src/bootloader/stage1/boot.asm
+++ src/bootloader/stage1/boot.asm
@@ -365,7 +365,7 @@ disk_reset:
 msg_loading:            db 'Loading...', ENDL, 0
 msg_read_failed:        db 'Read from disk failed!', ENDL, 0
 msg_kernel_not_found:   db 'KERNEL.BIN file not found!', ENDL, 0
-file_kernel_bin:        db 'KERNEL  BIN'
+file_kernel_bin:        db 'STAGE2  BIN'
 kernel_cluster:         dw 0
 
 KERNEL_LOAD_SEGMENT     equ 0x2000
--- tools/fat/fat.c
+++ tools/fat/fat.c
@@ -10,28 +10,28 @@ typedef uint8_t bool;
 
 typedef struct 
 {
-    uint8_t BootJumpInstruction[3];
-    uint8_t OemIdentifier[8];
-    uint16_t BytesPerSector;
-    uint8_t SectorsPerCluster;
-    uint16_t ReservedSectors;
-    uint8_t FatCount;
-    uint16_t DirEntryCount;
-    uint16_t TotalSectors;
-    uint8_t MediaDescriptorType;
-    uint16_t SectorsPerFat;
-    uint16_t SectorsPerTrack;
-    uint16_t Heads;
-    uint32_t HiddenSectors;
-    uint32_t LargeSectorCount;
+    uint8_t     BootJumpInstruction[3];
+    uint8_t     OemIdentifier[8];
+    uint16_t    BytesPerSector;
+    uint8_t     SectorsPerCluster;
+    uint16_t    ReservedSectors;
+    uint8_t     FatCount;
+    uint16_t    DirEntryCount;
+    uint16_t    TotalSectors;
+    uint8_t     MediaDescriptorType;
+    uint16_t    SectorsPerFat;
+    uint16_t    SectorsPerTrack;
+    uint16_t    Heads;
+    uint32_t    HiddenSectors;
+    uint32_t    LargeSectorCount;
 
     // extended boot record
-    uint8_t DriveNumber;
-    uint8_t _Reserved;
-    uint8_t Signature;
-    uint32_t VolumeId;          // serial number, value doesn't matter
-    uint8_t VolumeLabel[11];    // 11 bytes, padded with spaces
-    uint8_t SystemId[8];
+    uint8_t     DriveNumber;
+    uint8_t     _Reserved;
+    uint8_t     Signature;
+    uint32_t    VolumeId;          // serial number, value doesn't matter
+    uint8_t     VolumeLabel[11];    // 11 bytes, padded with spaces
+    uint8_t     SystemId[8];
 
     // ... we don't care about code ...
 
@@ -39,25 +39,25 @@ typedef struct
 
 typedef struct 
 {
-    uint8_t Name[11];
-    uint8_t Attributes;
-    uint8_t _Reserved;
-    uint8_t CreatedTimeTenths;
-    uint16_t CreatedTime;
-    uint16_t CreatedDate;
-    uint16_t AccessedDate;
-    uint16_t FirstClusterHigh;
-    uint16_t ModifiedTime;
-    uint16_t ModifiedDate;
-    uint16_t FirstClusterLow;
-    uint32_t Size;
+    uint8_t     Name[11];
+    uint8_t     Attributes;
+    uint8_t     _Reserved;
+    uint8_t     CreatedTimeTenths;
+    uint16_t    CreatedTime;
+    uint16_t    CreatedDate;
+    uint16_t    AccessedDate;
+    uint16_t    FirstClusterHigh;
+    uint16_t    ModifiedTime;
+    uint16_t    ModifiedDate;
+    uint16_t    FirstClusterLow;
+    uint32_t    Size;
 } __attribute__((packed)) DirectoryEntry;
 
 
-BootSector g_BootSector;
-uint8_t* g_Fat = NULL;
+BootSector      g_BootSector;
+uint8_t*        g_Fat = NULL;
 DirectoryEntry* g_RootDirectory = NULL;
-uint32_t g_RootDirectoryEnd;
+uint32_t        g_RootDirectoryEnd;
 
 
 bool readBootSector(FILE* disk)
